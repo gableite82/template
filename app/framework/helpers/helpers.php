6fd8ff93871e975d51359292ddfc1e77b1bb26ab
Gabriel Leite <gabrielleite82@hotmail.com>
2023-09-06 11:20:17 -0300

    inclusao view

--- app/framework/helpers/helpers.php
+++ app/framework/helpers/helpers.php
@@ -1,6 +1,8 @@
 <?php 
 
     use app\framework\classes\Router;
+    use app\framework\classes\Exception\Engine;
+    
 
     function path(){
         return $_SERVER['REQUEST_URI'];
@@ -16,6 +18,15 @@
             $routes = require '../app/routes/routes.php';
             $router = new Router;
             $router->execute($routes);
+        } catch (\Throwable $th) {
+            var_dump($th->getMessage());
+        }
+    }
+
+    function view(string $view, array $data = []){
+        try {
+            $engine = new Engine;
+            $engine->render($view, $data);
         } catch (\Throwable $th) {
             var_dump($th->getMessage());
         }
